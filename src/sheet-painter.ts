2724107dce82ef97aade251fefa345b4c6f5c536
Olin Kirkland <me@olinkirk.land>
2024-12-10 23:28:16 +0100

    Add the rest of the abilities

--- src/sheet-painter.ts
+++ src/sheet-painter.ts
@@ -1,6 +1,6 @@
-import { Action } from "./assets/data/data-types";
-import { Scoundrel } from "./scoundrel";
-import { getActionValue } from "./util/action-util";
+import { Action } from './assets/data/data-types';
+import { Scoundrel } from './scoundrel';
+import { getActionValue } from './util/action-util';
 
 import dataBackgrounds from './assets/data/background-details.json';
 import dataHeritages from './assets/data/heritage-details.json';
@@ -55,16 +55,53 @@ const data = {
         physicker: { x: 1097, y: 649.5 },
         saboteur: { x: 1097, y: 730 },
         venomous: { x: 1097, y: 787 },
-    }
+        // Lurk
+        infiltrator: { x: 1097, y: 269 },
+        ambush: { x: 1097, y: 323 },
+        daredevil: { x: 1097, y: 353 },
+        'the-devils-footsteps': { x: 1097, y: 430 },
+        expertise: { x: 1097, y: 532 },
+        'ghost-veil': { x: 1097, y: 610 },
+        reflexes: { x: 1097, y: 735.5 },
+        shadow: { x: 1097, y: 790 },
+        // Slide
+        'rooks-gambit': { x: 1097, y: 269 },
+        'cloak-and-dagger': { x: 1097, y: 330.5 },
+        'ghost-voice': { x: 1097, y: 444.5 },
+        'like-looking-into-a-mirror': { x: 1097, y: 532 },
+        'a-little-something-on-the-side': { x: 1097, y: 594 },
+        mesmerism: { x: 1097, y: 656 },
+        subterfuge: { x: 1097, y: 718 },
+        'trust-in-me': { x: 1097, y: 805.5 },
+        // Spider
+        foresight: { x: 1097, y: 269 },
+        calculating: { x: 1097, y: 327 },
+        connected: { x: 1097, y: 389 },
+        'functioning-vice': { x: 1097, y: 449.5 },
+        'ghost-contract': { x: 1097, y: 533 },
+        'jail-bird': { x: 1097, y: 617 },
+        mastermind: { x: 1097, y: 700 },
+        'weaving-the-web': { x: 1097, y: 784 },
+        // Whisper
+        compel: { x: 1097, y: 269 },
+        'ghost-mind': { x: 1097, y: 353 },
+        'iron-will': { x: 1097, y: 412 },
+        occultist: { x: 1097, y: 471 },
+        ritual: { x: 1097, y: 555 },
+        'strange-methods': { x: 1097, y: 640 },
+        tempest: { x: 1097, y: 724.5 },
+        warded: { x: 1097, y: 808.5 },
+    },
 };
 
-
-export async function paintSheet(scoundrel: Scoundrel, color: string): Promise<HTMLCanvasElement> {
+export async function paintSheet(
+    scoundrel: Scoundrel,
+    color: string
+): Promise<HTMLCanvasElement> {
     console.log('painting a sheet for a', scoundrel.playbook);
 
     if (!document.fonts.check('40px Gochi Hand'))
-        await new Promise(resolve => setTimeout(resolve, 2000));
-
+        await new Promise((resolve) => setTimeout(resolve, 2000));
 
     return new Promise((resolve, reject) => {
         const template = new Image();
@@ -86,17 +123,41 @@ export async function paintSheet(scoundrel: Scoundrel, color: string): Promise<H
             ctx.lineWidth = 2;
 
             // Heritage
-            const heritage = dataHeritages.find(h => h.id === scoundrel.heritageDetail);
+            const heritage = dataHeritages.find(
+                (h) => h.id === scoundrel.heritageDetail
+            );
             if (scoundrel.backgroundDetail)
-                ctx.fillText(heritage?.label || scoundrel.heritageDetail, data.heritageText.x, data.heritageText.y);
+                ctx.fillText(
+                    heritage?.label || scoundrel.heritageDetail,
+                    data.heritageText.x,
+                    data.heritageText.y
+                );
 
             // Background
-            const background = dataBackgrounds.find(bg => bg.id === scoundrel.backgroundDetail);
+            const background = dataBackgrounds.find(
+                (bg) => bg.id === scoundrel.backgroundDetail
+            );
             if (scoundrel.backgroundDetail)
-                ctx.fillText(background?.label || scoundrel.backgroundDetail, data.backgroundText.x, data.backgroundText.y);
+                ctx.fillText(
+                    background?.label || scoundrel.backgroundDetail,
+                    data.backgroundText.x,
+                    data.backgroundText.y
+                );
 
             // Fill in Action bubbles
-            ['hunt', 'study', 'survey', 'tinker', 'finesse', 'prowl', 'skirmish', 'wreck', 'attune', 'command', 'consort', 'sway'
+            [
+                'hunt',
+                'study',
+                'survey',
+                'tinker',
+                'finesse',
+                'prowl',
+                'skirmish',
+                'wreck',
+                'attune',
+                'command',
+                'consort',
+                'sway',
             ].forEach((action, i) => {
                 drawActionBubbleRow(ctx, scoundrel, action as Action);
             });
@@ -105,7 +166,8 @@ export async function paintSheet(scoundrel: Scoundrel, color: string): Promise<H
             scoundrel.abilities.forEach((ability) => {
                 // Draw one bubble for each ability
                 if (data.abilities.hasOwnProperty(ability)) {
-                    const point = data.abilities[ability as keyof typeof data.abilities];
+                    const point =
+                        data.abilities[ability as keyof typeof data.abilities];
                     drawBubble(ctx, point);
                 }
             });
@@ -119,16 +181,23 @@ export async function paintSheet(scoundrel: Scoundrel, color: string): Promise<H
     });
 }
 
-function drawActionBubbleRow(ctx: CanvasRenderingContext2D, scoundrel: Scoundrel, action: Action) {
+function drawActionBubbleRow(
+    ctx: CanvasRenderingContext2D,
+    scoundrel: Scoundrel,
+    action: Action
+) {
     // The horizontal offset between bubbles is 27px
     const bubbleCount = getActionValue(scoundrel.actions, action);
     // @ts-ignore
-    const point = data.actions[action] as { x: number, y: number };
+    const point = data.actions[action] as { x: number; y: number };
     for (let i = 0; i < bubbleCount; i++)
         drawBubble(ctx, { x: point.x + i * 27, y: point.y });
 }
 
-function drawBubble(ctx: CanvasRenderingContext2D, point: { x: number, y: number }) {
+function drawBubble(
+    ctx: CanvasRenderingContext2D,
+    point: { x: number; y: number }
+) {
     const radius = 8;
     ctx.beginPath();
     ctx.arc(point.x, point.y, radius, 0, 2 * Math.PI);
