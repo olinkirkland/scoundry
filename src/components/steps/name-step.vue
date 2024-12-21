9da9d7fbbd4991bc2035a2fc812dce26185cd822
olinkirkland <plakat@live.com>
2024-12-21 15:26:23 -0600

    Portraits

--- src/components/steps/name-step.vue
+++ src/components/steps/name-step.vue
@@ -35,13 +35,35 @@
             </button>
         </div>
     </div>
+    <div class="portraits-block">
+        <label>Choose a portrait</label>
+        <ul class="portraits-list">
+            <li
+                v-for="portrait in portraits"
+                :key="portrait.path"
+                @click="
+                    scoundrel.portrait == portrait.path
+                        ? (scoundrel.portrait = '')
+                        : (scoundrel.portrait = portrait.path)
+                "
+                :class="{ selected: scoundrel.portrait === portrait.path }"
+            >
+                <img :src="`/assets/${portrait.path}`" />
+            </li>
+        </ul>
+        <label v-if="scoundrel.portrait"
+            >{{ scoundrel.portrait }} ...
+            {{ getPortraitSource(scoundrel.portrait) }}</label
+        >
+    </div>
 </template>
 
 <script setup lang="ts">
+import looksData from '@/assets/data/looks.json';
+import namesData from '@/assets/data/names.json';
+import portraitsData from '@/assets/data/portraits.json';
 import StepHeader from '@/components/step-header.vue';
 import { Scoundrel } from '@/scoundrel';
-import namesData from '@/assets/data/names.json';
-import looksData from '@/assets/data/looks.json';
 import { randomItem } from '@/util/random';
 
 const props = defineProps<{
@@ -53,6 +75,7 @@ const lastNames = namesData.lastNames;
 const aliases = namesData.aliases;
 const adjectives = looksData.adjectives;
 const clothes = looksData.clothes;
+const portraits = portraitsData;
 
 function randomizeName() {
     props.scoundrel.name = `${randomItem(firstNames)} ${randomItem(lastNames)}`;
@@ -93,6 +116,12 @@ function joinWithAnd(arr: string[]) {
     if (arr.length === 2) return arr.join(' and ');
     return arr.slice(0, -1).join(', ') + ', and ' + arr.slice(-1);
 }
+
+function getPortraitSource(portraitPath: string) {
+    const source = portraits.find((p) => p.path === portraitPath)?.source;
+    if (source) return `(${source})`;
+    return 'No source';
+}
 </script>
 
 <style scoped lang="scss">
@@ -117,6 +146,60 @@ function joinWithAnd(arr: string[]) {
     flex-direction: column;
     align-items: center;
     gap: 1rem;
+}
+
+.portraits-block {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.4rem;
+    padding: 2rem;
+
+    ul.portraits-list {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        gap: 1rem;
+        max-width: 64rem;
+
+        > li {
+            width: 8rem;
+            height: 8rem;
+
+            cursor: pointer;
+            &.selected {
+                position: relative;
+                &::before {
+                    // primary color border
+                    content: '';
+                    position: absolute;
+                    border-radius: 50%;
+                    top: 0;
+                    left: 0;
+                    right: 0;
+                    bottom: 0;
+                    border: 0.2rem solid var(--color-primary);
+                    box-shadow: var(--shadow-md);
+                }
+            }
+
+            &:hover {
+                filter: brightness(1.1);
+            }
+
+            > img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                border-radius: 50%;
+                overflow: hidden;
+            }
+        }
+    }
+
+    > label:last-child {
+        margin-top: 1rem;
+    }
 }
 
 @media (max-width: 768px) {
