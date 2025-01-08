3011a3bc51686eec0d492683d22f0c5eb1803e71
olinkirkland <me@olinkirk.land>
2025-01-08 12:59:58 +0100

    Refactor localization structure by renaming English locale file and adding Spanish translations placeholder

--- src/i18n/locale.ts
+++ src/i18n/locale.ts
@@ -1,19 +1,20 @@
 import { createI18n, useI18n } from 'vue-i18n';
-import en from './locales/en.json';
-
+import en from './locales/en/translations.json';
+// import es from './locales/es/translations.json';
 
 // Type-define the master schema for the resource
-// If the other language entries (de, fr) are missing a key, it will be flagged as an error
+// If the other language entries (es, de) are missing a key, it will be flagged as an error
 type MessageSchema = typeof en;
 
-const i18n = createI18n<[MessageSchema], 'en'>({ // Add languages here
+const i18n = createI18n<[MessageSchema], 'en'>({
+    // Add languages here
     legacy: false, // you must set `false`, to use Composition API
     locale: 'en',
     fallbackLocale: 'en',
     messages: {
-        en,
+        en
         // es,
-    },
+    }
 });
 
 let i18nInstance: any;
