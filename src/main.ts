d50f8eadc3a7d0519a388c3825a4a1f2c1fddeb3
olinkirkland <me@olinkirk.land>
2025-01-24 09:16:38 +0100

    Increment version?

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import i18n from './i18n/locale';
 import { router } from './router';
 import { trackEvent } from './tracker';
 
-export const APP_VERSION = '1.3.0';
+export const APP_VERSION = '1.3.1';
 
 // Create the app
 const app = createApp(App);
