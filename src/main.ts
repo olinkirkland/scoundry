37a4fa3908c5476755cdea037da49286d926f0e0
Olin Kirkland <me@olinkirk.land>
2026-02-26 09:39:47 -0500

    Increment version

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import i18n from './i18n/locale';
 import { router } from './router';
 import { trackEvent } from './tracker';
 
-export const APP_VERSION = '1.3.3';
+export const APP_VERSION = '1.3.5';
 
 // Create the app
 const app = createApp(App);
