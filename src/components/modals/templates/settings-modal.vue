2feafcd8559109ac463a7aae10f86746c35ddd98
Olin Kirkland <me@olinkirk.land>
2026-08-09 00:02:52 -0400

    Minor styling

--- src/components/modals/templates/settings-modal.vue
+++ src/components/modals/templates/settings-modal.vue
@@ -211,7 +211,7 @@ section {
 
 .languages {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(8rem, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr));
     gap: 1rem;
 
     button.flag {
